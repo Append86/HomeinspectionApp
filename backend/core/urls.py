9fbee4108fbfccc254979ba2b5398a2aa39ea65e
GregB86 <matchplay2009@gmail.com>
2026-04-03 14:42:45 -0500

    Removed api from settings

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -35,8 +35,8 @@ router.register('photos', PhotoViewSet, basename='photos')
 urlpatterns = [
     path('admin8903inspect3/', admin.site.urls),
     path('', include(router.urls)),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 if settings.DEBUG:
