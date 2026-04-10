d7149b2684033f4d7f2c47e26115bdf94e8ccf1c
GregB86 <matchplay2009@gmail.com>
2026-04-10 05:19:57 -0500

    Make fix for pdf upload

--- backend/inspections/utils.py
+++ backend/inspections/utils.py
@@ -9,6 +9,10 @@ def render_to_pdf(template_src, context_dict={}):
     """
     template = get_template(template_src)
     html_string = template.render(context_dict)
+
+    # Passing the base_url from the request ensures images load via HTTPS
+    base_url = request.build_absolute_uri('/') if request else settings.MEDIA_URL
+    html = HTML(string=html_string, base_url=base_url)
     
     result = BytesIO()
     
